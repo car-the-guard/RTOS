1e42f64cf534c24050b030f832543c254b3b060f
SM_Park <moktak072@naver.com>
2026-01-03 14:29:58 +0900

    fix: 빌드 에러 제거

--- Core/Inc/CAN/can_bridge.h
+++ Core/Inc/CAN/can_bridge.h
@@ -8,6 +8,8 @@
 #ifndef INC_CAN_CAN_BRIDGE_H_
 #define INC_CAN_CAN_BRIDGE_H_
 
+#include "stm32f4xx_hal.h"
+#include "can.h"
 #include <stdint.h>
 
 typedef enum {
@@ -27,7 +29,7 @@ void CAN_send_sonar(uint16_t, uint16_t);
 void CAN_send_accel(uint16_t, uint16_t);
 void CAN_send_compass(uint16_t);
 
-void CAN_consume_rx_message(CAN_RxHeaderTypeDef RxHeader, CAN_payload_t payload);
+void CAN_consume_rx_message(CAN_RxHeaderTypeDef* RxHeader, CAN_payload_t payload);
 void CAN_receive_led_signal(uint8_t);
 
 #endif /* INC_CAN_CAN_BRIDGE_H_ */
